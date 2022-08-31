6cb3fb4d001c6ee2f5c753177a3839245ef53451
tezike <eziket@gmail.com>
2022-08-31 15:47:53 +0100

    .

---
+++ index.js
@@ -0,0 +1,65 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { WebClient } = require('@slack/web-api');
+const { buildSlackAttachments, formatChannelName } = require('./src/utils');
+
+(async () => {
+  try {
+    const channel = core.getInput('channel');
+    const status = core.getInput('status');
+    const color = core.getInput('color');
+    const messageId = core.getInput('message_id');
+    const captain = core.getInput('captain');
+    const command = core.getInput('command');
+    const token = process.env.SLACK_BOT_TOKEN;
+    const slack = new WebClient(token);
+
+    if (!channel && !core.getInput('channel_id')) {
+      core.setFailed(`You must provider either a 'channel' or a 'channel_id'.`);
+      return;
+    }
+
+    const attachments = buildSlackAttachments({ status, color, github, captain, command });
+    const channelId = core.getInput('channel_id') || (await lookUpChannelId({ slack, channel }));
+
+    if (!channelId) {
+      core.setFailed(`Slack channel ${channel} could not be found.`);
+      return;
+    }
+
+    const apiMethod = Boolean(messageId) ? 'update' : 'postMessage';
+
+    const args = {
+      channel: channelId,
+      attachments,
+    };
+
+    if (messageId) {
+      args.ts = messageId;
+    }
+
+    const response = await slack.chat[apiMethod](args);
+
+    core.setOutput('message_id', response.ts);
+  } catch (error) {
+    core.setFailed(error);
+  }
+})();
+
+async function lookUpChannelId({ slack, channel }) {
+  let result;
+  const formattedChannel = formatChannelName(channel);
+
+  // Async iteration is similar to a simple for loop.
+  // Use only the first two parameters to get an async iterator.
+  for await (const page of slack.paginate('conversations.list', { types: 'public_channel, private_channel' })) {
+    // You can inspect each page, find your result, and stop the loop with a `break` statement
+    const match = page.channels.find(c => c.name === formattedChannel);
+    if (match) {
+      result = match.id;
+      break;
+    }
+  }
+
+  return result;
+}
---
+++ src/utils.js
@@ -0,0 +1,74 @@
+const { context } = require('@actions/github');
+
+function buildSlackAttachments({ status, color, github, captain, command }) {
+  const { payload, ref, workflow, eventName } = github.context;
+  const { owner, repo } = context.repo;
+  const event = eventName;
+  const branch = event === 'pull_request' ? payload.pull_request.head.ref : ref.replace('refs/heads/', '');
+
+  const sha = event === 'pull_request' ? payload.pull_request.head.sha : github.context.sha;
+  const runId = parseInt(process.env.GITHUB_RUN_ID, 10);
+
+  const referenceLink =
+    event === 'pull_request'
+      ? {
+          title: 'Pull Request',
+          value: `<${payload.pull_request.html_url} | ${payload.pull_request.title}>`,
+          short: true,
+        }
+      : {
+          title: 'Branch',
+          value: `<https://github.com/${owner}/${repo}/commit/${sha} | ${branch}>`,
+          short: true,
+        };
+
+  return [
+    {
+      color,
+      fields: [
+        {
+          title: 'Repo',
+          value: `<https://github.com/${owner}/${repo} | ${owner}/${repo}>`,
+          short: true,
+        },
+        {
+          title: 'Workflow',
+          value: `<https://github.com/${owner}/${repo}/actions/runs/${runId} | ${workflow}>`,
+          short: true,
+        },
+        {
+          title: 'Status',
+          value: status,
+          short: true,
+        },
+        referenceLink,
+        {
+          title: 'Event',
+          value: event,
+          short: true,
+        },
+        {
+          title: 'Captain',
+          value: captain,
+          short: true,
+        },
+        {
+          title: 'Command',
+          value: command,
+          short: true,
+        },
+      ],
+      footer_icon: 'https://github.githubassets.com/favicon.ico',
+      footer: `<https://github.com/${owner}/${repo} | ${owner}/${repo}>`,
+      ts: Math.floor(Date.now() / 1000),
+    },
+  ];
+}
+
+module.exports.buildSlackAttachments = buildSlackAttachments;
+
+function formatChannelName(channel) {
+  return channel.replace(/[#@]/g, '');
+}
+
+module.exports.formatChannelName = formatChannelName;
